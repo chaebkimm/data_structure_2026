c4e2f06c778b0737c91ede185decd5dd80baefc5
chaebkimm <chae.b.kim@gmail.com>
2026-09-20 00:07:56 +0900

    Update lab.c

--- module_03_graph/student/lab.c
+++ module_03_graph/student/lab.c
@@ -1,29 +1,25 @@
 struct GraphNode {
 	char data;
 	int adj_list[10];
-	int adj_cap;
 	int adj_size;
 };
 
 struct GraphNode nodes[10];
-int nodes_cap = 10;
 int nodes_size = 0;
 
 void alphabet_nodes_init() {
-	nodes_size = nodes_cap;
-
-	for (int i = 0; i < nodes_cap; i++) {
+	nodes_size = 10;
+	for (int i = 0; i < 10; i++) {
 		nodes[i].data = 'A' + i;
-		nodes[i].adj_cap = 10;
 		nodes[i].adj_size = 0;
 	}
 }
 
 void edges_init() {
-	int edges[9][2] = {{0, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5},
-					   {3, 4}, {3, 5}, {4, 5}, {6, 7}};
-	int edges_count = 9;
-	for (int i = 0; i < edges_count; i++) {
+	int edges[9][2] = {
+		{0, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5},
+		{3, 4}, {3, 5}, {4, 5}, {6, 7}};
+	for (int i = 0; i < 9; i++) {
 		int u = edges[i][0];
 		int v = edges[i][1];
 		int u_adj_end = nodes[u].adj_size++;
@@ -33,7 +29,7 @@ void edges_init() {
 	}
 }
 
-int visited[10] = {0};
+int visited[10];
 
 void visited_init() {
 	for (int i = 0; i < nodes_size; i++) {
@@ -42,13 +38,13 @@ void visited_init() {
 }
 
 void graph_recursion(int i) {
-    char data_read = nodes[i].data;
-    for (int j = 0; j < nodes[i].adj_size; j++) {
-        int neighbor = nodes[i].adj_list[j];
-        if (visited[neighbor] == 0) {
-            visited[neighbor] = 1;
-            graph_recursion(neighbor);
-        }		
+	char data_read = nodes[i].data;
+	for (int j = 0; j < nodes[i].adj_size; j++) {
+		int adj = nodes[i].adj_list[j];
+		if (visited[adj] == 0) {
+			visited[adj] = 1;
+			graph_recursion(adj);
+		}
 	}
 }
 
@@ -57,16 +53,16 @@ int groups_count = 0;
 int current_group = 0;
 
 void search_connected_group(int i) {
-    group[i] = current_group;
+	group[i] = current_group;
 
-    for (int j = 0; j < nodes[i].adj_size; j++) {
-        int neighbor = nodes[i].adj_list[j];
+	for (int j = 0; j < nodes[i].adj_size; j++) {
+		int adj = nodes[i].adj_list[j];
 
-        if (visited[neighbor] == 0) {
-            visited[neighbor] = 1;
-            search_connected_group(neighbor);
-        }
-    }
+		if (visited[adj] == 0) {
+			visited[adj] = 1;
+			search_connected_group(adj);
+		}
+	}
 }
 
 void mark_all_groups() {
@@ -75,53 +71,53 @@ void mark_all_groups() {
 
 	for (int i = 0; i < nodes_size; i++) {
 		if (visited[i] == 0) {
-            visited[i] = 1;
+			visited[i] = 1;
 			current_group = groups_count++;
 			search_connected_group(i);
 		}
 	}
 }
 
-int search_num[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-int search_time = 0;
+int visit_num[10];
+int visit_time = 0;
 
-void save_search_num(int i) {
-	search_num[i] = search_time++;
+void save_visit_num(int i) {
+	visit_num[i] = visit_time++;
 	for (int j = 0; j < nodes[i].adj_size; j++) {
-		int neighbor = nodes[i].adj_list[j];
-		if (visited[neighbor] == 0) {
-            visited[neighbor] = 1;
-			save_search_num(neighbor);
+		int adj = nodes[i].adj_list[j];
+		if (visited[adj] == 0) {
+			visited[adj] = 1;
+			save_visit_num(adj);
 		}
 	}
 }
 
 int start_index = 0;
 
-void save_search_nums() {
+void save_visit_nums() {
 	visited_init();
-	search_time = 0;
-    visited[start_index] = 1;
-	save_search_num(start_index);
+	visit_time = 0;
+	visited[start_index] = 1;
+	save_visit_num(start_index);
 }
 
-int back_num[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-int parent[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+int back_num[10];
+int parent[10];
 
 void save_back_num(int i) {
-	back_num[i] = search_num[i];
+	back_num[i] = visit_num[i];
 	for (int j = 0; j < nodes[i].adj_size; j++) {
-		int neighbor = nodes[i].adj_list[j];
-		if (visited[neighbor] == 0) {
-            visited[neighbor] = 1;
-			parent[neighbor] = i;
-			save_back_num(neighbor);
-			if (back_num[i] > back_num[neighbor]) {
-				back_num[i] = back_num[neighbor];
+		int adj = nodes[i].adj_list[j];
+		if (visited[adj] == 0) {
+			visited[adj] = 1;
+			parent[adj] = i;
+			save_back_num(adj);
+			if (back_num[i] > back_num[adj]) {
+				back_num[i] = back_num[adj];
 			}
-		} else if (neighbor != parent[i]) {
-			if (back_num[i] > search_num[neighbor]) {
-				back_num[i] = search_num[neighbor];
+		} else if (adj != parent[i]) {
+			if (back_num[i] > visit_num[adj]) {
+				back_num[i] = visit_num[adj];
 			}
 		}
 	}
@@ -129,7 +125,7 @@ void save_back_num(int i) {
 
 void save_back_nums() {
 	visited_init();
-    visited[start_index] = 1;
-    parent[start_index] = -1;
+	visited[start_index] = 1;
+	parent[start_index] = -1;
 	save_back_num(start_index);
 }
